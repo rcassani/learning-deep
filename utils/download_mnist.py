534031e3a9b3278a840e6b6330bf705576b1393c
Raymundo Cassani <raymundo.cassani@gmail.com>
2019-04-26 13:28:57 -0400

    update MNIST downloader

--- utils/download_mnist.py
+++ utils/download_mnist.py
@@ -5,18 +5,35 @@ Downloads MNIST dataset and creates an HDF5 for 'train', 'validation' and 'test'
 """
 
 import urllib.request
-import pickle as pickle
+import pickle
 import gzip
-import os
-import numpy as np
+import h5py
+import os 
 
+# make mnist directory
+try:
+    os.mkdir('../data/mnist/')
+except OSError:
+    pass
+        
 
-path = 'http://deeplearning.net/data/mnist'
-mnist_filename_all = 'mnist.pkl'
-local_filename = os.path.join(args.savedir, mnist_filename_all)
-urllib.request.urlretrieve(
-    "{}/{}.gz".format(path,mnist_filename_all), local_filename+'.gz')
-tr,va,te = pickle.load(gzip.open(local_filename+'.gz','r'), encoding='latin1')
-np.save(open(local_filename+'.npy','w'), (tr,va,te))
-        
-        
+# download MNIST
+ulr_mnist = 'http://deeplearning.net/data/mnist/mnist.pkl.gz'
+mnist_filename = 'mnist.pkl.gz'
+urllib.request.urlretrieve(ulr_mnist, '../data/mnist/' + mnist_filename)
+      
+# 'mnist.pkl.gz' was created in Python2, 
+#  thus  'latin1' encoding is needed in read it in Python3
+with gzip.open('../data/mnist/mnist.pkl.gz', 'rb') as f:
+    train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+
+# Save each set as an individual HDF5 file     
+datasets = [train_set, valid_set, test_set, test_set]
+dataset_names = ['train', 'validation', 'test']
+
+for dataset, dataset_name in zip(datasets, dataset_names):
+    print(dataset_name)  
+    file = h5py.File('../data/mnist/' + dataset_name + '.hdf5', 'w')
+    file.create_dataset('samples', data=dataset[0])
+    file.create_dataset('labels' , data=dataset[1])
+    file.close

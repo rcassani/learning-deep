46fabdb33e22eb73f44d13af0c70d06bf7429739
Raymundo Cassani <raymundo.cassani@gmail.com>
2019-04-30 16:00:03 -0400

    MNIST dataloader relocated and udpdated

--- utils/dataloaders_mnist_ram.py
+++ utils/dataloaders_mnist_ram.py
@@ -6,13 +6,16 @@ Data is kept in RAM
 import torch
 import pickle, gzip
 import numpy as np
+import os 
 from torch.utils.data import Dataset
+
 
 class Loader(Dataset):
     def __init__(self, set_type=None):
         super(Loader, self).__init__()
         # Import MNIST data
-        with gzip.open('../data/mnist/mnist.pkl.gz', 'rb') as f:
+        dir_this_script = os.path.dirname(os.path.realpath(__file__))
+        with gzip.open(dir_this_script  + '/../data/mnist/mnist.pkl.gz', 'rb') as f:
             train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
         if set_type == 'train':         
             # 50K samples for training

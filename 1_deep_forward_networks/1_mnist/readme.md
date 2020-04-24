ddb376ee4902e2f8c71fa0299cbd4df8f2f26d70
Raymundo Cassani <raymundo.cassani@gmail.com>
2020-04-23 21:27:07 -0400

    Update MLP to classify MNIST

---
+++ 1_deep_forward_networks/1_mnist/readme.md
@@ -0,0 +1,38 @@
+# MLP to classify MNIST dataset
+
+## Architectures
+The used architecture or model is described in [`model_zoo.py`](model_zoo.py)) with the name `mlp_simple`. It is a n MLP with layer sizes: [784, 25, 10, 10], this is to say, 2 hidden layers. ReLU was used as activation function.
+
+## Training the MLP
+The model can be trained from the terminal with the command:  
+```bash
+python -i train_dnn.py --epochs=50 --ngpus=1 --lr=0.5 --l2=1 --model=mlp_simple
+```
+alternatively, `the final_state.pt` file can be loaded as:
+```bash
+python -i train_dnn.py --ngpus=1 --model=mlp_simple --final_state=final_state_mlp_simple.pt
+```
+
+## Learning and Accuracy curves
+Both models were trained with a learning rate of 0.5, L2 of 1, a maximun number of 50 epochs, SGD as optimizer and cross cross entropy as loss function. Below the plots for loss and accuracy vs epochs.
+
+|   |   |
+|---|---|
+| <img src="https://user-images.githubusercontent.com/8238803/80164966-55e08980-85a8-11ea-84f0-62c81b3bbdd3.png" style="width: 400px;"/> | <img src="https://user-images.githubusercontent.com/8238803/80164976-5bd66a80-85a8-11ea-9c6b-a4e625924fce.png" style="width: 400px;"/> |
+| loss | accuracy |
+
+## Examples of classification
+These are some examples from the test set, [T]rue and [P]redicted labels are indicated.
+
+<center>
+<img src="https://user-images.githubusercontent.com/8238803/80164991-6690ff80-85a8-11ea-9a75-1df8b8d5183f.png" style="width: 100%;"/><br>
+Classification results with `mlp_simple` model
+</center>  
+
+## Plotting some weights
+Lastly, these are the plots for the weights between the input layer and the first hidden layer
+
+<center>
+<img src="https://user-images.githubusercontent.com/8238803/80164987-642ea580-85a8-11ea-9e99-f9cc02f3db77.png" style="width: 100%;"/><br>
+Weights `W1` in `mlp_simple` model
+</center>  
--- 1_deep_forward_networks/1_mnist/train_dnn.py
+++ 1_deep_forward_networks/1_mnist/train_dnn.py
@@ -11,6 +11,7 @@ from torch.utils.data.dataloader import DataLoader
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 import model_zoo                         # model to train 
 
@@ -21,11 +22,9 @@ from dataloaders_mnist_ram import Loader
 from learning_loop import LearningLoop    # general learning loop
 
 # Terminal: 
-# python -i train_dnn.py --epochs=20 --ngpus=1 --lr=0.5 --l2=1 --model=mlp_notebook
+# python -i train_dnn.py --epochs=100 --ngpus=1 --lr=0.5 --l2=1 --model=mlp_simple
 # Spyder terminal
-# runfile('train_dnn.py', '--epochs=20 --ngpus=1 --lr=0.5 --l2=1 --model=mlp_notebook')
-
-# runfile('train_dnn.py', '--epochs=50 --ngpus=1 --lr=1 --l2=1 --model=mlp_notebook --train-batch-size=50000')
+# runfile('train_dnn.py', '--epochs=100 --ngpus=1 --lr=0.5 --l2=1 --model=mlp_simple')
 
 
 # Training settings
@@ -44,17 +43,19 @@ parser.add_argument('--l2', type=float, default=0.00001, metavar='lambda', help=
 
 parser.add_argument('--ngpus', type=int, default=0, help='Number of GPUs to use. Default=0 (no GPU)')
 
-parser.add_argument('--checkpoint-path', type=str, default='./checkpoints', metavar='PATH', help='Directory for checkpoint files, if None or empty string, checkpoints are not saved')
-parser.add_argument('--checkpoint-load', type=int, default=None, metavar='N', help='Indicates epoch to restart. If None, training starts from scratch')
+parser.add_argument('--ckpt-path', type=str, default='./ckpts', metavar='PATH', help='Directory for checkpoint files, if None or empty string, checkpoints are not saved')
+parser.add_argument('--ckpt-load', type=int, default=None, metavar='N', help='Indicates epoch to restart. If None, training starts from scratch')
 
 parser.add_argument('--seed', type=int, default=1, metavar='N', help='random seed (default: 1)')
 parser.add_argument('--n-workers', type=int, default=1, metavar='N', help='Number of workers for dataloaders')
 
+parser.add_argument('--final-state', action='store_true', help='If this flag is present and the final state file exists, it is loaded')
+
 args = parser.parse_args()
 
-# validate checkpoint-path input
-if args.checkpoint_path == '' or args.checkpoint_path == 'None':
-  args.checkpoint_path = None
+# validate ckpt-path input
+if args.ckpt_path == '' or args.ckpt_path == 'None':
+  args.ckpt_path = None
 
 # Verify CUDA
 cuda_flag = args.ngpus > 0 and torch.cuda.is_available()
@@ -63,10 +64,7 @@ print('CUDA Mode is: ' +  str(cuda_flag))
 # Random seed
 torch.manual_seed(args.seed)
 if cuda_flag:
-    torch.cuda.manual_seed(args.seed)
-
-# Loader will put all the dataset in RAM
-ram = True
+  torch.cuda.manual_seed(args.seed)
 
 # Datasets and DataLoaders
 train_dataset = Loader('train')
@@ -75,7 +73,7 @@ test_dataset  = Loader('test')
 
 train_loader = DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle=True, num_workers=args.n_workers)
 valid_loader = DataLoader(valid_dataset, batch_size=args.valid_batch_size, shuffle=True, num_workers=args.n_workers)
-test_loader  = DataLoader(test_dataset , batch_size=args.test_batch_size , shuffle=True, num_workers=args.n_workers)
+test_loader  = DataLoader(test_dataset , batch_size=args.test_batch_size , shuffle=False, num_workers=args.n_workers)
 
 # 1. Model design and GPU capability
 print('Model = ' + args.model)
@@ -83,51 +81,80 @@ model = getattr(model_zoo, args.model)()
 
 # Load model in GPU(s)
 if cuda_flag:
-    if args.ngpus > 1:
-        model = torch.nn.DataParallel(model, device_ids=list(range(args.ngpus)))
-    else:
-        model = model.cuda()
+  if args.ngpus > 1:
+    model = torch.nn.DataParallel(model, device_ids=list(range(args.ngpus)))
+  else:
+    model = model.cuda()
 
 # 2. Loss and Optimizer
 optimizer = optim.SGD(model.parameters(), lr=args.lr)
 loss_funct = torch.nn.CrossEntropyLoss()
 
-# 3. Create training loop and train
-learner = LearningLoop(model, optimizer, loss_funct,
+if not (args.final_state and os.path.exists('./final_state_' + args.model + '.pt')):
+  # 3. Create training loop and train
+  learner = LearningLoop(args.model, model, optimizer, loss_funct,
                        train_loader=train_loader, valid_loader=valid_loader,
-                       checkpoint_path=args.checkpoint_path, checkpoint_load=args.checkpoint_load,
+                       ckpt_path=args.ckpt_path, ckpt_load=args.ckpt_load,
                        cuda_flag=cuda_flag)
 
-# Train
-learner.train(n_epochs=args.epochs, patience = args.patience)
+  # Train
+  learner.train(n_epochs=args.epochs, patience = args.patience)
 
 # Recreate the model, load final state and test
-tester = LearningLoop(model, optimizer, loss_funct,
+tester = LearningLoop(args.model, model, optimizer, loss_funct,
                       test_loader=test_loader, cuda_flag=cuda_flag)
 
 # Load final state
-tester.load_state_file('./final_state.pt')
+tester.load_state_file( './final_state_' + args.model + '.pt')
 
 # Test
-tester.test()
+y_hat_tensor = tester.test()
+y_hat = y_hat_tensor.cpu().numpy()
 
+#%% Plots  
 # Ploting loss vs iterations
 plt.figure()
 ix = np.arange(tester.ix_epoch)
 plt.plot(ix, np.array(tester.history['train_loss']))
+plt.plot(ix, np.array(tester.history['valid_loss']))
+plt.legend(['training set', 'validation set'])
+plt.xlabel('epochs')
+plt.ylabel('loss')
 
-#%% Plotting some weight
+# Ploting acc vs iterations
+plt.figure()
+ix = np.arange(tester.ix_epoch)
+plt.plot(ix, np.array(tester.history['train_acc']))
+plt.plot(ix, np.array(tester.history['valid_acc']))
+plt.legend(['training set', 'validation set'])
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+
+# True and Predicted labels for some examples
+examples = np.random.randint(10000, size=40)
+plt.figure()
+for ix_example in range(len(examples)):
+  index = examples[ix_example]
+  X = test_dataset.samples[index]
+  y = test_dataset.labels[index] 
+  tmp = np.reshape(X, [28,28])
+  ax = plt.subplot(5,8, ix_example + 1)
+  ax.set_yticklabels([])
+  ax.set_xticklabels([])
+  plt.title('T'+ str(y) + ', P' + str(y_hat[index]))
+  plt.imshow(tmp, cmap='gray')
+  
 # A. Weights from Input layer to Hidden layer 1
 w1 = tester.model.fc1.weight.data.cpu().numpy()
 plt.figure()
 for ix_w in range(25):
     tmp = np.reshape(w1[ix_w,:], [28,28])
-    ax = plt.subplot(5,5, ix_w + 1)
+    ax = plt.subplot(5, 5, ix_w + 1)
     ax.set_yticklabels([])
     ax.set_xticklabels([])
     plt.title(str(ix_w))
     plt.imshow(1- tmp, cmap='gray')    
-    
+  
 # B. Weights from Hidden layer 1 to Hidden layer 2    
 w2 = tester.model.fc2.weight.data.cpu().numpy()
 plt.figure()
